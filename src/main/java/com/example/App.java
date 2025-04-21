956b9f40b7aa8e701845bb89f3caeec94268f189
Barret22 <vviktoroduvan@gmail.com>
2025-04-21 21:31:42 +0200

    create project Maven

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -3,6 +3,7 @@ package com.example;
 public class App {
     public static void main(String[] args) {
         String password = PasswordGenerator.generatePassword(12);
-        System.out.println("Пароль з клієнтського проєкту: " + password);
+        System.out.println("Пароль з " +
+                "клієнтського проєкту: " + password);
     }
 }

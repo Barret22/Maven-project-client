d636504dc0174821a6145c3f64b99161b32ef986
Barret22 <vviktoroduvan@gmail.com>
2025-04-21 21:28:02 +0200

    create project

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,8 @@
+package com.example;
+
+public class App {
+    public static void main(String[] args) {
+        String password = PasswordGenerator.generatePassword(12);
+        System.out.println("Пароль з клієнтського проєкту: " + password);
+    }
+}
